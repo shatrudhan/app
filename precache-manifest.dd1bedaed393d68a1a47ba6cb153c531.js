b88266e24756849edd24eff18eb573a9ccd36127
shatrudhan <sonuchoudhary0@gmail.com>
2019-12-13 12:50:27 +0530

    Updates

---
+++ precache-manifest.dd1bedaed393d68a1a47ba6cb153c531.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cd580610cde945b608e9a557e6a3e8",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "0ac7be992523fb1064cc",
+    "url": "/app/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "07408bf96e94dcb2d25e",
+    "url": "/app/static/css/main.e4ec092a.chunk.css"
+  },
+  {
+    "revision": "0ac7be992523fb1064cc",
+    "url": "/app/static/js/2.82c99677.chunk.js"
+  },
+  {
+    "revision": "7479810abfe166676dccdce909ac1ed9",
+    "url": "/app/static/js/2.82c99677.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07408bf96e94dcb2d25e",
+    "url": "/app/static/js/main.2790f4c2.chunk.js"
+  },
+  {
+    "revision": "79a8bcd269d7a4604983",
+    "url": "/app/static/js/runtime-main.faa214fc.js"
+  }
+]);
